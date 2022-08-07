93cb317d6fd5b4d8f96344602c26ea759b8dfee8
Sanjeeva Reddy Nandela <sanju.nandela@gmail.com>
2022-08-07 11:58:09 +0530

    removed scenedelegate file and added testcases for viewmodel and service stubs

--- NewsAroundTests/Stubs/NewsAPIServiceProtocolStub.swift
+++ NewsAroundTests/Stubs/NewsAPIServiceProtocolStub.swift
@@ -6,30 +6,18 @@
 //
 
 import XCTest
+@testable import NewsAround
 
-class NewsAPIServiceProtocolStub: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+class NewsAPIServiceProtocolStub: NewsAPIServiceProtocol {
+    
+    var serviceFailed = false
+    
+    func fetchNews(currentPage: Int, onCompletion: @escaping (NewsResponse?, NetworkError?) -> ()) {
+//        if let _ = error {
+//            serviceFailed = true
+//        }else {
+//            serviceFailed = false
+//        }
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
+        
 }
